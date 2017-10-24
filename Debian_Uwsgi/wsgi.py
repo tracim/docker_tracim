d1537e5c1a7de3b18d0b3a7f51dd2626afa0ab26
Adrien Panay <algooapy@users.noreply.github.com>
2017-10-24 17:20:06 +0200

    Fixes app path in wsgi script
    Closes #5

--- Debian_Uwsgi/wsgi.py
+++ Debian_Uwsgi/wsgi.py
@@ -5,8 +5,8 @@ APP_CONFIG = '{{ instance.tracim_config_file_path }}'
 # Setup logging
 import logging
 import logging.config
-logging.config.fileConfig('/tmp/config.ini')
+logging.config.fileConfig('/tracim/tracim/config.ini')
 
 from paste.deploy import loadapp
-application = loadapp('config:/tmp/config.ini')
+application = loadapp('config:/tracim/tracim/config.ini')
 application.debug = False
