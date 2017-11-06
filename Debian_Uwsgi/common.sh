97d3ac4d424792d5608906e1b646bf57199e60ce
Adrien Panay <algooapy@users.noreply.github.com>
2017-11-06 14:57:13 +0100

    Cleans up quotes

--- Debian_Uwsgi/common.sh
+++ Debian_Uwsgi/common.sh
@@ -18,7 +18,7 @@ if [ ! -f /etc/tracim/config.ini ]; then
     CONFIG_FILE_IS_NEW=1
     cp /tracim/tracim/development.ini.base /etc/tracim/config.ini
     sed -i 's/\(8080$\)/80/' /etc/tracim/config.ini
-    sed -i 's/\(depot_storage_dir *= *\).*/depot_storage_dir = \/var\/tracim\/depot/' /etc/tracim/config.ini
+    sed -i "s/\(depot_storage_dir *= *\).*/depot_storage_dir = \/var\/tracim\/depot/" /etc/tracim/config.ini
     sed -i "s/\(radicale.server.filesystem.folder *= *\).*/radicale.server.filesystem.folder = \/var\/tracim\/radicale/" /etc/tracim/config.ini
     SECRET=$(python -c "import uuid; print(str(uuid.uuid4()))")
     sed -i "s/\(cookie_secret *= *\).*/cookie_secret = $SECRET/" /etc/tracim/config.ini
